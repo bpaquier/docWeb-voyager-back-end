7bbf9852e9ba6b1c8ea8ba9eaa10605c1a536927
bastien17-dev <bastien.paquier@gmail.com>
2020-06-12 08:55:55 +0200

    test-pull

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($_SERVER['SERVER_NAME'] === 'localhost'){
 }
 
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("voyager2");
+    $response->getBody()->write("voyager3");
     return $response;
 })->setName('Home');
 
