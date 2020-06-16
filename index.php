a6644ecb47e91109eae990a63e648f2685fce89b
bastien17-dev <bastien.paquier@gmail.com>
2020-06-16 18:19:02 +0200

    change text on index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($_SERVER['SERVER_NAME'] === 'localhost'){
 }
 
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('test');
+    $response->getBody()->write('Voyager API');
     return $response;
 })->setName('Home');
 
